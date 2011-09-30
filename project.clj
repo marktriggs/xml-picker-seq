784896dd9ffe4173a0a272dc876d9365a9d7676f
Mark Triggs <mark@dishevelled.net>
2011-10-01 06:58:24 +1000

    Release version 0.0.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject xml-picker-seq "0.0.2-SNAPSHOT"
+(defproject xml-picker-seq "0.0.2"
   :description "A Clojure wrapper for reading large XML files with XOM."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [xom/xom "1.2.5"]]
